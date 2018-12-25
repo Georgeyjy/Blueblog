6d39e6c2d109cf8beb55b3491403b507c6bd6c83
George Yang <george_yjy@163.com>
2018-12-25 16:47:12 +0800

    used factory function to initialize

--- Blueblog/bluelog/__init__.py
+++ Blueblog/bluelog/__init__.py
@@ -2,57 +2,82 @@ import os
 
 import click
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-from flask_moment import Moment
-from flask_bootstrap import Bootstrap
-from flask_ckeditor import CKEditor
-from flask_mail import Mail
+
 
 from bluelog.blueprints.admin import admin_bp
 from bluelog.blueprints.auth import auth_bp
 from bluelog.blueprints.blog import blog_bp
+from bluelog.extensions import db, moment, bootstrap, ckeditor, mail
+from bluelog.settings import config
 
 
-app = Flask('blueblog')
-app.config.from_pyfile('settings.py')
+def create_app(config_name=None):
+    if config_name is None:
+        config_name = os.getenv('FLASK_CONFIG', 'development')
 
-db = SQLAlchemy(app)
-moment = Moment(app)
-bootstrap = Bootstrap(app)
-ckeditor = CKEditor(app)
-mail = Mail(app)
+    app = Flask('bluelog')
+    app.config.from_object(config[config_name])
 
-app.register_blueprint(admin_bp, url_prefix='/admin')
-app.register_blueprint(auth_bp, url_prefix='/auth')
-app.register_blueprint(blog_bp)
+    register_logging(app)
+    register_extensions(app)
+    register_blueprints(app)
+    register_shell_context(app)
+    register_template_context(app)
+    register_errors(app)
+    register_commands(app)
 
-
-@app.shell_context_processor
-def make_shell_context():
-    return dict(db=db)
-
-
-@app.errorhandler(400)
-def bad_request(e):
-    return render_template('errors/400.html'), 400
+    return app
 
 
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('errors/404.html'), 404
+def register_logging(app):
+    pass
 
 
-@app.errorhandler(500)
-def internal_server_error(e):
-    return render_template('errors/500.html'), 500
+def register_extensions(app):
+    bootstrap.init_app(app)
+    db.init_app(app)
+    moment.init_app(app)
+    ckeditor.init_app(app)
+    mail.init_app(app)
 
 
-@app.cli.command()
-@click.option('--drop', is_flag=True, help='Create after drop')
-def initdb(drop):
-    if drop:
-        click.confirm('Confirm to delete the database?', abort=True)
-        db.drop_all()
-        click.echo('Deleted database')
-    db.create_all()
-    click.echo('Initialized database')
+def register_blueprints(app):
+    app.register_blueprint(admin_bp, url_prefix='/admin')
+    app.register_blueprint(auth_bp, url_prefix='/auth')
+    app.register_blueprint(blog_bp)
+
+
+def register_shell_context(app):
+    @app.shell_context_processor
+    def make_shell_context():
+        return dict(db=db)
+
+
+def register_template_context(app):
+    pass
+
+
+def register_errors(app):
+    @app.errorhandler(400)
+    def bad_request(e):
+        return render_template('errors/400.html'), 400
+
+    @app.errorhandler(404)
+    def page_not_found(e):
+        return render_template('errors/404.html'), 404
+
+    @app.errorhandler(500)
+    def internal_server_error(e):
+        return render_template('errors/500.html'), 500
+
+
+def register_commands(app):
+    @app.cli.command()
+    @click.option('--drop', is_flag=True, help='Create after drop')
+    def initdb(drop):
+        if drop:
+            click.confirm('Confirm to delete the database?', abort=True)
+            db.drop_all()
+            click.echo('Deleted database')
+        db.create_all()
+        click.echo('Initialized database')
