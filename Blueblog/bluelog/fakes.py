c15eb72f45553ee2a9e7b6db3db20142d8788b13
George Yang <george_yjy@163.com>
2018-12-26 20:53:46 +0800

    added functions to generate fake info

---
+++ Blueblog/bluelog/fakes.py
@@ -0,0 +1,103 @@
+import random
+
+from sqlalchemy.exc import IntegrityError
+from faker import Faker
+
+from bluelog.models import Admin, Category, Post, Comment
+from bluelog.extensions import db
+
+fake = Faker()
+
+
+def fake_admin():
+    admin = Admin(
+        username='admin',
+        blog_title='Bluelog',
+        blog_subtitle='a blue blog',
+        name='Scar Face',
+        about='Veni vidi vici'
+    )
+    admin.set_password('fakeadmin')
+    db.session.add(admin)
+    db.session.commit()
+
+
+def fake_category(count=10):
+
+    category = Category(name='Default')
+    db.session.add(category)
+
+    for i in range(count):
+        category = Category(name=fake.word())
+        db.session.add(category)
+        try:
+            db.session.commit()
+        except IntegrityError:
+            db.session.rollback()
+
+
+def fake_post(count=50):
+    for i in range(count):
+        post = Post(
+            title=fake.sentence(),
+            body=fake.text(2000),
+            category=Category.query.get(random.randint(1, Category.query.count())),
+            timestamp=fake.date_time_this_year()
+        )
+        db.session.add(post)
+    db.session.commit()
+
+
+def fake_comments(count=500):
+    for i in range(count):
+        comment = Comment(
+            author=fake.name(),
+            email=fake.email(),
+            site=fake.url(),
+            body=fake.sentence(),
+            timestamp=fake.date_time_this_year(),
+            reviewed=True,
+            post=Post.query.get(random.randint(1, Post.query.count()))
+        )
+        db.session.add(comment)
+
+    salt = int(count * 0.1)
+    for i in range(salt):
+        # unreviewed comments
+        comment = Comment(
+            author=fake.name(),
+            email=fake.email(),
+            site=fake.url(),
+            body=fake.sentence(),
+            timestamp=fake.date_time_this_year(),
+            reviewed=False,
+            post=Post.query.get(random.randint(1, Post.query.count()))
+        )
+        db.session.add(comment)
+        # admin comments
+        comment = Comment(
+            author='Admin',
+            email='admin@xxx.com',
+            site='xx.com',
+            body=fake.sentence(),
+            timestamp=fake.date_time_this_year(),
+            from_admin=True,
+            reviewed=True,
+            post=Post.query.get(random.randint(1, Post.query.count()))
+        )
+        db.session.add(comment)
+    db.session.commit()
+    for i in range(salt):
+        # replies
+        comment = Comment(
+            author=fake.name(),
+            email=fake.email(),
+            site=fake.url(),
+            body=fake.sentence(),
+            timestamp=fake.date_time_this_year(),
+            reviewed=True,
+            reply=Comment.query.get(random.randint(1, Comment.query.count())),
+            post=Post.query.get(random.randint(1, Post.query.count()))
+        )
+        db.session.add(comment)
+    db.session.commit()
