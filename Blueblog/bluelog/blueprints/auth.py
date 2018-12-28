1a4c795da834446962165288325396b49ef54a5a
George Yang <george_yjy@163.com>
2018-12-28 23:39:20 +0800

    added pages

--- Blueblog/bluelog/blueprints/auth.py
+++ Blueblog/bluelog/blueprints/auth.py
@@ -1,11 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth_bp = Blueprint('auth', __name__)
 
 
 @auth_bp.route('/login', methods=['GET', 'POST'])
 def login():
-    return 'admin login page'
+    return render_template('auth/login.html')
 
 
 @auth_bp.route('/logout')
