60e9e07cc52bc29e47883dedaab39f18d36b68c1
George Yang <george_yjy@163.com>
2019-01-03 19:58:43 +0800

    added some views

--- Blueblog/bluelog/blueprints/admin.py
+++ Blueblog/bluelog/blueprints/admin.py
@@ -1,10 +1,13 @@
-from flask import Blueprint, flash, redirect, url_for, render_template, request, current_app
+import os
+
+from flask import Blueprint, flash, redirect, url_for, render_template, request, current_app, send_from_directory
+from flask_ckeditor import upload_fail, upload_success
 from flask_login import login_required, current_user
 
 from bluelog.extensions import db
-from bluelog.forms import SettingForm, PostForm
-from bluelog.models import Post, Category, Comment
-from bluelog.utils import redirect_back
+from bluelog.forms import SettingForm, PostForm, CategoryForm, LinkForm
+from bluelog.models import Post, Category, Comment, Link
+from bluelog.utils import redirect_back, allowed_file
 
 admin_bp = Blueprint('admin', __name__)
 
@@ -99,16 +102,146 @@ def set_comment(post_id):
 @admin_bp.route('/comment/manage')
 @login_required
 def manage_comment():
-    pass
+    filter_rule = request.args.get('filter', 'all')
+    page = request.args.get('page', 1, type=int)
+    per_page = current_app.config['BLUE_LOG_COMMENT_PER_PAGE']
+    if filter_rule == 'unread':
+        filtered_comments = Comment.query.filter_by(reviewed=False)
+    if filter_rule == 'admin':
+        filtered_comments = Comment.query.filter_by(from_admin=True)
+    else:
+        filtered_comments = Comment.query
+    pagination = filtered_comments.order_by(Comment.timestamp.desc()).paginate(page, per_page=per_page)
+    comments = pagination.items
+    return render_template('admin/manage_comments.html', comments=comments, pagination=pagination)
 
 
 @admin_bp.route('/comment/<int:comment_id>/approve', methods=['POST'])
 @login_required
-def manage_post():
-    pass
+def approve_comment(comment_id):
+    comment = Comment.query.get_or_404(comment_id)
+    comment.reviewed = True
+    db.session.commit()
+    flash('Comment published', 'success')
+    return redirect_back()
 
 
 @admin_bp.route('/comment/<int:comment_id>/delete', methods=['POST'])
 @login_required
-def manage_post():
-    pass
+def manage_post(comment_id):
+    comment = Comment.query.get_or_404(comment_id)
+    db.session.delete(comment)
+    db.session.commit()
+    flash('Comment deleted', 'success')
+    return redirect_back()
+
+
+@admin_bp.route('/category/manage')
+@login_required
+def manage_category():
+    return render_template('admin/manage_category.html')
+
+
+@admin_bp.route('/category/new', methods=['GET', 'POST'])
+@login_required
+def new_category():
+    form = CategoryForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        category = Category(name=name)
+        db.session.add(category)
+        db.session.commit()
+        flash('Category created', 'success')
+        return redirect(url_for('.manage_category'))
+    return render_template('admin/new_category.html', form=form)
+
+
+@admin_bp.route('/category/<int:category_id>/edit', methodd=['GET','POST'])
+@login_required
+def edit_category(category_id):
+    form = CategoryForm()
+    category = Category.query.get_or_404(category_id)
+    if category.id == 1:
+        flash('You are not allowed to edit Default category', 'warning')
+        return redirect(url_for('.manage_category'))
+    if form.validate_on_submit():
+        category.name = form.name.data
+        db.session.commit()
+        flash('Category updated', 'success')
+        return redirect(url_for('.manage_category'))
+    form.name.data = category.name
+    return render_template('admin/edit_category.html', form=form)
+
+
+@admin_bp.route('/category/<int:category_id>/edit', methodd=['POST'])
+@login_required
+def delete_category(category_id):
+    category = Category.query.get_or_404(category_id)
+    if category.id == 1:
+        flash('You are not allowed to delete Default category', 'warning')
+    db.session.delete(category)
+    db.commit()
+    flash('Category deleted', 'success')
+    return redirect(url_for('.manage_category'))
+
+
+@admin_bp.route('/link/manage')
+@login_required
+def manage_link():
+    return render_template('admin/manage_link.html')
+
+
+@admin_bp.route('link/new', methods=['GET', 'POST'])
+@login_required
+def new_link():
+    form = LinkForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        url = form.url.data
+        link = Link(name=name, url=url)
+        db.session.add(link)
+        db.commit()
+        flash('New link created.', 'success')
+        return redirect(url_for('.manage_link'))
+    return render_template('admin/new_link.html', form=form)
+
+
+@admin_bp.route('link/<int:link_id>/edit', methods=['GET', 'POST'])
+@login_required
+def edit_link(link_id):
+    form = LinkForm()
+    link = Link.query.get_or_404(link_id)
+    if form.validate_on_submit():
+        link.name = form.name.data
+        link.url = form.url.data
+        db.session.commit()
+        flash('Link updated.', 'success')
+        return redirect(url_for('.manage_link'))
+    form.name.data = link.name
+    form.url.data = link.url
+    return render_template('admin/edit_link.html', form=form)
+
+
+@admin_bp.route('/link/<int:link_id>/delete', methods=['POST'])
+@login_required
+def delete_link(link_id):
+    link = Link.query.get_or_404(link_id)
+    db.session.delete(link)
+    db.session.commit()
+    flash('Link deleted.', 'success')
+    return redirect(url_for('.manage_link'))
+
+
+@admin_bp.route('/uploads/<path:filename>')
+def get_image(filename):
+    return send_from_directory(current_app.config['BLUELOG_UPLOAD_PATH'], filename)
+
+
+@admin_bp.route('/upload', methods=['POST'])
+def upload_image():
+    f = request.files.get('upload')
+    if not allowed_file(f.filename):
+        return upload_fail('Image only!')
+    f.save(os.path.join(current_app.config['BLUELOG_UPLOAD_PATH'], f.filename))
+    url = url_for('.get_image', filename=f.filename)
+    return upload_success(url, f.filename)
